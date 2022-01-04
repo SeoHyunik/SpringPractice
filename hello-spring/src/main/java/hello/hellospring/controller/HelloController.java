0eb3b33cede2b61c61eb4ff2178b39767569e795
SeoHyunik <hos0917@naver.com>
2022-01-04 15:18:35 +0900

    commit 3 from gram

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,21 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    
+
     @GetMapping("hello") // localhost:8080/hello
-    public String hello (Model model) {
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!"); // key = data , val = hello!!
         return "hello"; // resources/templates안의 html 파일명과 동일함.
     }
+
+    @GetMapping("hello-mvc") // localhost:8080/hello
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
